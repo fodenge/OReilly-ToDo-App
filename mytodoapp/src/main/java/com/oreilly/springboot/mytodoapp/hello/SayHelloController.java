5fe9a79658bd48d530bf4da7a20893e53597592b
Shruti <shrutipandeyofficial@gmail.com>
2025-02-11 14:10:06 +0530

    Add Hard-coded HTML response

--- mytodoapp/src/main/java/com/oreilly/springboot/mytodoapp/hello/SayHelloController.java
+++ mytodoapp/src/main/java/com/oreilly/springboot/mytodoapp/hello/SayHelloController.java
@@ -11,4 +11,20 @@ public class SayHelloController {
     public String sayHello(){
         return "Hello! Have a good day ahead";
     }
+    @GetMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>");
+        sb.append("My First HTML page");
+        sb.append("</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("Welcome to my First Hard-coded HTML page. Wish me luck");
+        sb.append("</body>");
+        sb.append("</html>");
+        return sb.toString();
+    }
 }
